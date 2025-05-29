915a513be6879e77926cdcaaf7d68b6fa1814666
Andrei Provotorov <a.provotorov@cbooster.ru>
2025-05-29 10:49:49 +0300

    Выполнить задачу

--- src/index.php
+++ src/index.php
@@ -3,38 +3,60 @@
 namespace App;
 
 function search(array $docs, string $query): array {
-    $queryWords = preg_split('/\s+/', normalize($query), -1, PREG_SPLIT_NO_EMPTY);
-    $result = [];
+    $index = buildInvertedIndex($docs);
+    $docWordFreqs = [];
+    $docLengths = [];
+    $totalDocs = count($docs);
 
     foreach ($docs as $doc) {
+        $id = $doc['id'];
         $normalizedText = normalize($doc['text']);
         $words = preg_split('/\s+/', $normalizedText, -1, PREG_SPLIT_NO_EMPTY);
+        $docWordFreqs[$id] = array_count_values($words);
+        $docLengths[$id] = count($words);
+    }
 
-        $wordCounts = array_count_values($words);
-        $matchedWordsCount = 0;
-        $totalHits = 0;
+    $queryWords = preg_split('/\s+/', normalize($query), -1, PREG_SPLIT_NO_EMPTY);
+    $scores = [];
 
-        foreach ($queryWords as $word) {
-            if (isset($wordCounts[$word])) {
-                $matchedWordsCount++;
-                $totalHits += $wordCounts[$word];
-            }
+    foreach ($queryWords as $word) {
+        if (!isset($index[$word])) {
+            continue;
         }
 
-        if ($matchedWordsCount > 0) {
-            $result[] = [
-                'id' => $doc['id'],
-                'matched' => $matchedWordsCount,
-                'hits' => $totalHits,
-            ];
+        $idf = log($totalDocs / count($index[$word]));
+
+        foreach ($index[$word] as $docId) {
+            $tf = ($docWordFreqs[$docId][$word] ?? 0) / $docLengths[$docId];
+            $tfIdf = $tf * $idf;
+
+            if (!isset($scores[$docId])) {
+                $scores[$docId] = 0;
+            }
+            $scores[$docId] += $tfIdf;
         }
     }
 
-    usort($result, function ($a, $b) {
-        return [$b['matched'], $b['hits']] <=> [$a['matched'], $a['hits']];
-    });
+    arsort($scores);
 
-    return array_column($result, 'id');
+    return array_keys($scores);
+}
+
+function buildInvertedIndex(array $docs): array {
+    $index = [];
+
+    foreach ($docs as $doc) {
+        $id = $doc['id'];
+        $normalizedText = normalize($doc['text']);
+        $words = preg_split('/\s+/', $normalizedText, -1, PREG_SPLIT_NO_EMPTY);
+        $uniqueWords = array_unique($words);
+
+        foreach ($uniqueWords as $word) {
+            $index[$word][] = $id;
+        }
+    }
+
+    return $index;
 }
 
 function normalize(string $text): string {
